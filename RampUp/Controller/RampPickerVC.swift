6f9e8370ba705881bfd53f2def979fe65e0a76bd
vsukiasyan <vsukiasyan1@hotmail.com>
2018-05-07 22:49:31 -0700

    UIGestureRecognizer used to detect tap so you can pick the ramps to place on the previous view controller

--- RampUp/Controller/RampPickerVC.swift
+++ RampUp/Controller/RampPickerVC.swift
@@ -13,6 +13,7 @@ class RampPickerVC: UIViewController {
     
     var sceneView: SCNView!
     var size: CGSize!
+    weak var rampPlacerVC: RampPlacerVC!
     
     init(size: CGSize) {
         super.init(nibName: nil, bundle: nil)
@@ -32,21 +33,55 @@ class RampPickerVC: UIViewController {
         sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         view.insertSubview(sceneView, at: 0)
         
+        preferredContentSize = size
+        
         let scene = SCNScene(named: "art.scnassets/Ramps.scn")!
         sceneView.scene = scene
-        
+        // Set up camera
         let camera = SCNCamera()
         camera.usesOrthographicProjection = true
         scene.rootNode.camera = camera
         
-        let obj = SCNScene(named: "art.scnassets/pipe.dae")
-        let node = obj?.rootNode.childNode(withName: "pipe", recursively: true)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        sceneView.addGestureRecognizer(tap)
+        
+        let rotate = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(0.01 * Double.pi), z: 0, duration: 0.1))
+        
+        // Set up ramp objects to display
+        var obj = SCNScene(named: "art.scnassets/pipe.dae")
+        var node = obj?.rootNode.childNode(withName: "pipe", recursively: true)
+        node?.runAction(rotate)
         node?.scale = SCNVector3Make(0.0028, 0.0028, 0.0028)
         node?.position = SCNVector3Make(-1, 0.7, -1)
         scene.rootNode.addChildNode(node!)
         
+        obj = SCNScene(named:  "art.scnassets/pyramid.dae")
+        node = obj?.rootNode.childNode(withName: "pyramid", recursively: true)
+        node?.runAction(rotate)
+        node?.scale = SCNVector3Make(0.005, 0.005, 0.005)
+        node?.position = SCNVector3Make(-1, -0.5, -1)
+        scene.rootNode.addChildNode(node!)
         
-        preferredContentSize = size
+        obj = SCNScene(named:  "art.scnassets/quarter.dae")
+        node = obj?.rootNode.childNode(withName: "quarter", recursively: true)
+        node?.runAction(rotate)
+        node?.scale = SCNVector3Make(0.0058, 0.0058, 0.0058)
+        node?.position = SCNVector3Make(-1, -2.45, -1)
+        scene.rootNode.addChildNode(node!)
+
+        
+        
+    }
+    
+    @objc func handleTap(_ gestureRecognizer: UIGestureRecognizer) {
+        let p = gestureRecognizer.location(in: sceneView)
+        let hitResults = sceneView.hitTest(p, options: [:])
+        
+        if hitResults.count > 0 {
+            let node = hitResults[0].node
+            rampPlacerVC.onRampSelected(node.name!)      
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
