f85822cabbf593ef156cfc0063a8f9f2bbe8683a
vsukiasyan <vsukiasyan1@hotmail.com>
2018-05-07 22:04:39 -0700

    Added assets, ARKit, etc.

---
+++ RampUp/Controller/RampPickerVC.swift
@@ -0,0 +1,68 @@
+//
+//  RampPickerVC.swift
+//  RampUp
+//
+//  Created by Vic Sukiasyan on 5/7/18.
+//  Copyright © 2018 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+    
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.size = size
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        let scene = SCNScene(named: "art.scnassets/Ramps.scn")!
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        let obj = SCNScene(named: "art.scnassets/pipe.dae")
+        let node = obj?.rootNode.childNode(withName: "pipe", recursively: true)
+        node?.scale = SCNVector3Make(0.0028, 0.0028, 0.0028)
+        node?.position = SCNVector3Make(-1, 0.7, -1)
+        scene.rootNode.addChildNode(node!)
+        
+        
+        preferredContentSize = size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
